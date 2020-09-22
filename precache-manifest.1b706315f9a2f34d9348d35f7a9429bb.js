6c73c0e871fff9ac356a2ade50de74a01eace076
Nikola Kokic <nikolazaraznestvari@gmail.com>
2020-09-23 00:04:10 +0200

    Updates

---
+++ precache-manifest.1b706315f9a2f34d9348d35f7a9429bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eed38891e8735a6a51a43f742094ddf",
+    "url": "/react-amazon-clone/index.html"
+  },
+  {
+    "revision": "894f13de900e68388cea",
+    "url": "/react-amazon-clone/static/css/main.29a6f702.chunk.css"
+  },
+  {
+    "revision": "a0046fcec43c7ba7d206",
+    "url": "/react-amazon-clone/static/js/2.7007efea.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/react-amazon-clone/static/js/2.7007efea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "894f13de900e68388cea",
+    "url": "/react-amazon-clone/static/js/main.5d7fb0ad.chunk.js"
+  },
+  {
+    "revision": "47e484e8257be41a7b02",
+    "url": "/react-amazon-clone/static/js/runtime-main.768d7aac.js"
+  }
+]);
